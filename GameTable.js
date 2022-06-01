4a36eb9b78f74006430618f2e1702ff87cc0619f
Piotr Bernaś <bercyk@gmail.com>
2022-06-01 22:58:16 +0200

    game table

---
+++ GameTable.js
@@ -0,0 +1,26 @@
+import Hero from "./Hero.js"
+
+
+
+class GameTable{
+
+    constructor(){
+        
+        this.playerDeck = this.getPlayerDeckHtml()
+        this.fightingCreature = this.getFightCreatureHtml(dataHero)
+        this.attackSpell = this.getAttackSpellHtml()
+        this.passiveSpell = this.getPassiveSpellHtml()
+        
+        this.summaryAttackScore = this.getSummaryAttackScoreHtml()
+
+        getFightCreatureHtml(dataHero){
+            
+        }
+        
+
+    }
+    
+
+}
+
+export default GameTable
